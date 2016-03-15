ab49a70a2c355f20b8c3779a80c8810fddb20f48
yanorlov <hotkeydo@gmail.com>
2016-03-15 21:26:19 +0300

    project prepare

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,37 @@
-/**
- * Created by - on 15.03.2016.
- */
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
 
 gulp.task('hello', function() {
     console.log('Hello Zell');
+});
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+})
+
+var useref = require('gulp-useref');
+
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
 });
